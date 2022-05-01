45a504fa03ec402ef19a1f34a72fb1d19c02f434
Mikew1999 <mikewalters1234@gmail.com>
2022-05-01 23:39:41 +0000

    Added table locking model

--- home/models.py
+++ home/models.py
@@ -3,13 +3,6 @@ import uuid
 from django.db import models
 
 
-# Tracks site visit numbers
-class SiteVisits(models.Model):
-    ''' A table to track site visit numbers '''
-    date = models.DateField()
-    site_visits = models.BigIntegerField()
-
-
 # Tracks what a user has clicked on in this session
 class SiteActionsTaken(models.Model):
     ''' A table to track what a user has clicked on '''
@@ -17,3 +10,98 @@ class SiteActionsTaken(models.Model):
         primary_key=True, default=uuid.uuid4, editable=False)
     ip_address = models.CharField(max_length=50)
     actions_taken = models.TextField()
+
+
+# Tracks site errors
+class SiteErrors(models.Model):
+    ''' A table to track errors '''
+    session_id = models.ForeignKey(SiteActionsTaken, on_delete=models.CASCADE)
+    last_actions_taken = models.TextField()
+    error_message = models.CharField(max_length=255)
+
+
+class LockingManager(models.Manager):
+    """ Add lock/unlock functionality to manager.
+
+    Example::
+
+        class Job(models.Model):
+
+            manager = LockingManager()
+
+            counter = models.IntegerField(null=True, default=0)
+
+            @staticmethod
+            def do_atomic_update(job_id)
+                ''' Updates job integer, keeping it below 5 '''
+                try:
+                    # Ensure only one HTTP request can do this update at once.
+                    Job.objects.lock()
+
+                    job = Job.object.get(id=job_id)
+                    # If we don't lock the tables two simultanous
+                    # requests might both increase the counter
+                    # going over 5
+                    if job.counter < 5:
+                        job.counter += 1
+                        job.save()
+
+                finally:
+                    Job.objects.unlock()
+
+
+    """
+
+    def lock(self):
+        """ Lock table.
+
+        Locks the object model table so that atomic update is possible.
+        Simulatenous database access request pend until the lock is unlock()'ed.
+
+        Note: If you need to lock multiple tables, you need to do lock them
+        all in one SQL clause and this function is not enough. To avoid
+        dead lock, all tables must be locked in the same order.
+
+        See http://dev.mysql.com/doc/refman/5.0/en/lock-tables.html
+        """
+        cursor = connection.cursor()
+        table = self.model._meta.db_table
+        logger.debug("Locking table %s" % table)
+        cursor.execute("LOCK TABLES %s WRITE" % table)
+        row = cursor.fetchone()
+        return row
+
+    def unlock(self):
+        """ Unlock the table. """
+        cursor = connection.cursor()
+        table = self.model._meta.db_table
+        cursor.execute("UNLOCK TABLES")
+        row = cursor.fetchone()
+        return row
+
+
+# Tracks site visit numbers
+class SiteVisits(models.Model):
+    ''' A table to track site visit numbers '''
+    manager = LockingManager()
+
+    @staticmethod
+      def do_atomic_update()
+          ''' Updates job integer, keeping it below 5 '''
+           try:
+                # Ensure only one HTTP request can do this update at once.
+                Job.objects.lock()
+
+                job = Job.object.get(id=)
+                # If we don't lock the tables two simultanous
+                # requests might both increase the counter
+                # going over 5
+                if job.counter < 5:
+                    job.counter += 1
+                    job.save()
+
+            finally:
+                Job.objects.unlock()
+
+    date = models.DateField()
+    site_visits = models.BigIntegerField()
--- home/views.py
+++ home/views.py
@@ -1,9 +1,29 @@
 ''' Home page handling '''
+from datetime import date
+
 from django.shortcuts import render
-# Create your views here.
+from .models import SiteActionsTaken, SiteVisits, SiteErrors
+
+
+def find_ip(request):
+    ''' Finds IP address of user '''
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip_addr = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip_addr = request.META.get('REMOTE_ADDR')
+    return ip_addr
 
 
 def index(request):
     ''' home page '''
     # Site tracking
+    today = date.today()
+    ip_addresses = SiteVisits.objects.all()
+    users_ip = find_ip(request)
+
+    if users_ip not in ip_addresses:
+        SiteVisits(date=today, site_visits='')
+
+    if 
     return render(request, 'home/index.html')
